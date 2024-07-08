948f5ca54b1a8fe814c793121a57dcd150dd2fdc
Nazar Huliiev <nazar.huliev@gmail.com>
2024-07-08 16:25:31 +0300

    Set default parameter for GetAllRestaurantsQuery

--- Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuery.cs
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuery.cs
@@ -9,9 +9,9 @@ public class GetAllRestaurantsQuery : IRequest<PagedResult<RestaurantDto>>
 {
     public string SearchPhrase { get; set; } = string.Empty;
 
-    public int PageSize { get; set; }
+    public int PageSize { get; set; } = 5;
 
-    public int PageNumber { get; set; }
+    public int PageNumber { get; set; } = 1;
 
     public string? SortBy { get; set; }
 
